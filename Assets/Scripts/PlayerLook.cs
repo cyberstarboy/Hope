b20c9ef0049b810827a000608acd18abf53416c4
“Doug <doug@christgames.org>
2018-06-12 00:14:26 -0500

    Character Movement

--- Assets/Scripts/PlayerLook.cs
+++ Assets/Scripts/PlayerLook.cs
@@ -2,13 +2,60 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerLook : MonoBehaviour {
+public class PlayerLook : MonoBehaviour
+{
+    public Transform playerBody;
+    public float mouseSensitivity;
 
-	void Update () {
-        RotateCamera();
-	}
+    float xAxisClamp = 0.0f;
 
-    void RotateCamera() {
-        
+    void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
     }
+
+    void Update()
+    {
+        RotateCamera();       
+    }
+
+    void RotateCamera()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        float rotAmountX = mouseX * mouseSensitivity;
+        float rotAmountY = mouseY * mouseSensitivity;
+
+        xAxisClamp -= rotAmountY;
+
+        Vector3 targetRotCam = transform.rotation.eulerAngles;
+        Vector3 targetRotBody = playerBody.rotation.eulerAngles;
+
+        targetRotCam.x -= rotAmountY;
+        targetRotCam.z = 0;
+        targetRotBody.y += rotAmountX;
+
+        if(xAxisClamp > 90)
+        {
+            xAxisClamp = 90;
+            targetRotCam.x = 90;
+        }
+        else if(xAxisClamp < -90)
+        {
+            xAxisClamp = -90;
+            targetRotCam.x = 270;
+        }
+
+        print(mouseY);
+
+
+        transform.rotation = Quaternion.Euler(targetRotCam);
+        playerBody.rotation = Quaternion.Euler(targetRotBody);
+
+
+    }
+
+
+
 }
