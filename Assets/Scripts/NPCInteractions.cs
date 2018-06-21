0bd23cec2fd52ea73867404f75e3782d3f798b39
Irma De Leon <idlministry@yahoo.com>
2018-06-21 14:48:50 -0500

    added Interactions for Pearl

--- Assets/Scripts/NPCInteractions.cs
+++ Assets/Scripts/NPCInteractions.cs
@@ -3,34 +3,67 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NPCInteractions : MonoBehaviour {
+    [SerializeField] GameObject cameraCanvas = null;
     [SerializeField] GameObject dialogue = null;
-    [SerializeField] GameObject tracts = null;
+    [SerializeField] GameObject tract = null;
     [SerializeField] GameObject colorPlane = null;
+    [SerializeField] int scoreVal = 0;
     [SerializeField] int dialogueSeconds = 0;
+    public int currentScore = 0;
+    public int maxScore = 0;
+    public int triggerVal;
 
-	// Use this for initialization
-	void Start () {
-		
+    // Use this for initialization
+    void Start () {
+        maxScore = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if ((Input.GetKeyDown(KeyCode.Space)) && 
+            (triggerVal == scoreVal))
+        {
+            StartCoroutine(ShowDialogue());
+        }
 	}
 
-    private void OnTriggerEnter(Collider other)
+    private IEnumerator ShowDialogue()
     {
-        Debug.Log(other.gameObject.name);
+        Debug.Log("ShowDialogue; about to Activate Interactions for: ");
+        Debug.Log(scoreVal);
         dialogue.SetActive(true);
-        tracts.SetActive(true);
-        colorPlane.SetActive(true);
+        yield return new WaitForSecondsRealtime(dialogueSeconds);
+        dialogue.SetActive(false);
+        StartCoroutine(ActivateInteractions());
     }
 
-    private void OnTriggerExit(Collider other)
+    private IEnumerator ActivateInteractions()
     {
-        Debug.Log(other.gameObject.name);
+        Debug.Log("Activating Interactions for: ");
+        Debug.Log(scoreVal);
+        tract.SetActive(true);
+        yield return new WaitForSecondsRealtime(dialogueSeconds);
+        currentScore = currentScore + scoreVal;
+        if (currentScore >= maxScore)
+        {
+            //Show score and head to church to end the game
+            Debug.Log("Go back to church and end the game");
+        }
+        else
+        {
+            Debug.Log(currentScore);
+        }
+
+        DeactivateInteractions();
+    }
+
+    private void DeactivateInteractions()
+    {
+        Debug.Log("Dectivating Interactions for: ");
+        Debug.Log(scoreVal);
         dialogue.SetActive(false);
-        tracts.SetActive(false);
+        tract.SetActive(false);
         colorPlane.SetActive(false);
+        cameraCanvas.SetActive(false);
     }
 }
