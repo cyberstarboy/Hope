2d0f20c2d498ecca2b3b31d473e175a0fbd9af94
Irma De Leon <idlministry@yahoo.com>
2018-06-20 23:01:39 -0500

    added NPCInteraction Script and materials

---
+++ Assets/Scripts/NPCInteractions.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCInteractions : MonoBehaviour {
+    [SerializeField] GameObject dialogue = null;
+    [SerializeField] GameObject tracts = null;
+    [SerializeField] GameObject colorPlane = null;
+    [SerializeField] int dialogueSeconds = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.name);
+        dialogue.SetActive(true);
+        tracts.SetActive(true);
+        colorPlane.SetActive(true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log(other.gameObject.name);
+        dialogue.SetActive(false);
+        tracts.SetActive(false);
+        colorPlane.SetActive(false);
+    }
+}
