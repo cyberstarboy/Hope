e39babd03f9b43c0f4e6965c8ce04a5b6829ed86
“Doug <doug@christgames.org>
2018-06-23 11:52:10 -0500

    Splash Screen

---
+++ Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SplashScreen : MonoBehaviour
+{
+    public Image splashImage;
+    public string loadLevel;
+
+    IEnumerator Start()
+    {
+        splashImage.canvasRenderer.SetAlpha(0.0f);
+        splashImage.CrossFadeAlpha(1.0f, 1.5f, false);
+        yield return new WaitForSeconds(10.0f);
+        SceneManager.LoadScene(loadLevel);
+    }
+}
