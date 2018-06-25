dc7fa0af2dcd732679b8025fb67178faa93fb773
“Doug <doug@christgames.org>
2018-06-24 22:47:01 -0500

    Updated Scenes and End Scripts

---
+++ Assets/Scripts/EnterChurch.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(AudioSource))]
+public class EnterChurch : MonoBehaviour {
+
+    public AudioClip click;
+    AudioSource audioSource;
+
+    public string scene;
+
+    [HideInInspector]
+    public Shader shader1;
+    [HideInInspector]
+    public Shader shader2;
+    [HideInInspector]
+    public Renderer rend;
+
+	void Start () 
+    {
+        audioSource = GetComponent<AudioSource>();
+        rend = GetComponent<Renderer>();
+        shader1 = Shader.Find("Standard");
+        shader2 = Shader.Find("Legacy Shaders/VertexLit");
+	}
+
+    void OnMouseDown()
+    {
+        audioSource.PlayOneShot(click, 0.7F);
+    }
+
+    void OnMouseUpAsButton()
+    {
+         Globals.IsEndGame = true;
+         SceneManager.LoadScene(scene);
+    }
+
+    void OnMouseOver()
+    {
+        rend.material.shader = shader2;
+    }
+
+    void OnMouseExit()
+    {
+        rend.material.shader = shader1;
+    }
+}
--- Assets/Scripts/NPCLookAt.cs
+++ Assets/Scripts/NPCLookAt.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 public class NPCLookAt : MonoBehaviour {
 
     public Transform target;
-    public float speed = 5f;
+    public float speed = 3f;
 
-	void Update () 
+	void Update ()
     {
-        Vector3 direction = target.position - transform.position;
-        Quaternion rotation = Quaternion.LookRotation(direction);
-        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, speed * Time.deltaTime);
+        if (Vector3.Distance(target.position, this.transform.position) < 8)
+        {
+       
+            Vector3 direction = target.position - transform.position;
+
+            Vector3 delta = new Vector3(direction.x, 0f, direction.z);
+            Quaternion rotation = Quaternion.LookRotation(delta);
+
+          /*  Quaternion rotation = Quaternion.LookRotation(direction); */
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, speed * Time.deltaTime);
+        }
 	}
 }
